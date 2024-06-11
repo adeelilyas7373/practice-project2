b669843ed2a9a3dcd8f2d73e14ccd525c763b095
haseeb-vaconai <haseeb@vacon.ai>
2024-06-11 00:54:58 -0700

    App changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HI Adeel ilyas
         </a>
       </header>
     </div>
